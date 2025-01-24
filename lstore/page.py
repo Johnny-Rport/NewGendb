61a3625c6c99782fbfe64b3e3d7e668618c07d9d
Johnny <jar@overlord.lan>
2025-01-23 22:33:09 -0800

    Implementing pages

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,29 @@
+import pickle
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = {}
 
     def has_capacity(self):
-        pass
+        # Returns size of dictionary as bytes
+        return len(pickle.dumps(self.data))
 
     def write(self, value):
         self.num_records += 1
+        
+        # Size of current bytes in page
+        size_data = self.has_capacity()
+        
+        # Size of value being inserted
+        size_value = len(pickle.dumps(value))
+
+        # 4096 Bytes is ideal
+        if(size_data + size_value) >= 4096:
+            # Make another page handled by table.py
+            return "Error Code???"
+        else:
+            self.data[len(self.data)+1] = value
         pass
 
