ae70a4e5dac53c9c5d41d455908a18e5da1608dd
Johnny <jar@overlord.lan>
2025-02-18 23:32:31 -0800

    Finished implementation and Added documentation

--- lstore/page_directory.py
+++ lstore/page_directory.py
@@ -1,56 +1,143 @@
 from lstore.page import Page
 
 # Job is to track the pages
-#TODO: Metadata
+"""
+page_list -> All data
+tail_pages -> For updates
+meta_pages -> Has the following:
+    version
+    tail_v (Tail version)
+    min_rid
+    max_rid
+"""
+
+#TODO: INDEXING TO GAIN VERSIONING or DOUBLE POITERS METHOD
 class Page_directory:
     def __init__(self, num_columns):
         self.num_columns = num_columns
         self.pageid_counter = []
         self.page_list = {}
-        self.meta_pages = [] # Pending
+        self.tail_pages = [] # [0 1 2 3]
+        self.meta_pages = [] # [0 1 2 3]
         self.initialize()
         pass
+    
+    """
+    * Function Name: initialize
+    * Purpose: Creates the initial set of pages
+    * Parameter: None
+    * Return: None
+    """
 
     def initialize(self):
-        #Create meta page
+        self.make_meta() # Make meta and tail page
         for pageid in range(self.num_columns):
             self.pageid_counter.append(pageid)
             self.make_page(pageid)
         pass
 
-    def expand(self):
+    """
+    * Function Name: expand
+    * Purpose: Increase amount of pages
+    * Parameter: rid -> int
+    * Return: None
+    """
+
+    def expand(self, rid):
+        self.make_meta(rid) # Make meta and tail page
         for index in range(self.num_columns):
             self.pageid_counter[index] += self.num_columns
             index = self.pageid_counter[index]
             self.make_page(index)
         pass
 
+    """
+    * Function Name: make_meta
+    * Purpose: Make the meta and tail page
+    * Parameter: min_rid -> int
+    * Return: None
+    """
+
+    def make_meta(self, min_rid=0):
+        page = Page()
+        self.tail_pages.append(page)
+
+        meta = {"min_rid": min_rid, "max_rid": min_rid+1}
+        self.meta_pages.append(meta)
+        pass
+
+    """
+    * Function Name: make_page
+    * Purpose: make a generic page, id it, and insert into page_list
+    * Parameter: pageid -> int from pageid_counter
+    * Return: None
+    """
+
     def make_page(self, pageid):
         page = Page()
         self.page_list[pageid] = page
         pass
-    
-    def get_tail(self, column):
+
+    """
+    * Function Name: get_base_tail
+    * Purpose: Grabs the most recent base pages
+    * Parameter: column -> int
+    * Return: page in specified column
+    """
+
+    def get_base_tail(self, column):
         id = self.pageid_counter[column]
         return self.page_list[id]
 
-    #TODO: Figure out a way to add meta data
-    def meta_data(self):
-        pass
+    """
+    * Function Name: get_base
+    * Purpose: Find the base page of specified rid
+    * Parameter: rid -> int, i -> iterator
+    * Return: The RID's Base page
+    """
 
+    def get_base(self, rid, i):
+        index = self.meta_pages.index(self.meta(rid))
+        offset = index*5
+        return self.page_list[i+offset]
 
+    """
+    * Function Name: tail
+    * Purpose: Find the tail page for RID
+    * Parameter: rid -> int
+    * Return: tail page for RID
+    """
+
+    def tail(self, rid):
+        index = self.meta_pages.index(self.meta(rid))
+        return self.tail_pages[index]
+
+    """
+    * Function Name: meta
+    * Purpose: Find the meta page with the specified rid value
+    * Parameter: rid -> int
+    * Return: Meta page of RID
+    """
+
+    def meta(self, rid):
+        for meta_page in self.meta_pages:
+            if(meta_page["min_rid"] <= rid <= meta_page["max_rid"]):
+                return meta_page
+
+#  Version | Version_Tail | MIN rid range | MAX rid range 
+    
 #   FOR BASE PAGES
 #   example: 5 columns
 #   i += 5, for each page increment by 5 for easy book keeping
 
-# --Indirection | ----------------- |
-#   meta1       | Page 0  | Page 1  | and so on
-#   meta2       | Page 5  | Page 6  |
-#   meta3       | Page 10 | Page 11 |
-#   meta4       | Page 15 | Page 16 |
-#   meta5       | Page 20 | Page 21 |
+# --Indirection | --Column Pages--- |
+#   meta0-tail  | Page 0  | Page 1  | and so on
+#   meta1-tail  | Page 5  | Page 6  |
+#   meta2-tail  | Page 10 | Page 11 |
+#   meta3-tail  | Page 15 | Page 16 |
+#   meta4-tail  | Page 20 | Page 21 |
 
-# Using the list, the base tail is always being pointed at, for easy appending
-# For querying use meta data to choose the page for RID
-#??? What goes into the metadata?
-# metaID | pointer to tail page RID | Version | RID range 
+# metaID is connected to index
+# Meta page and tail page are connected through the index
+# Whether to check the base page or tail page is based on the version and version_tail
+# Debating the dirty boolean because we can merge when the tail page is full
--- lstore/table.py
+++ lstore/table.py
@@ -1,7 +1,7 @@
 from lstore.index import Index
 from lstore.page import Page
 from lstore.page_directory import Page_directory
-from time import time
+from datetime import datetime
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -24,7 +24,6 @@ class Table:
     :param key: int             #Index of table key in columns
     """
 
-    #TODO: Imlpemenet page_directory class to store pages
     def __init__(self, name, num_columns, key):
         self.name = name
         self.key = key
@@ -38,37 +37,86 @@ class Table:
         print("merge is happening")
         pass
 
+
+    """
+    * Function Name: insert_record
+    * Purpose: Insert record into page_directory
+    * Parameter: data -> List of desired data to insert
+    * Return: None
+    """
+
     def insert_record(self, data: list):
         record = Record(self.rid, data[0], data[1:])
         try:
-            self.page_directory.get_tail(0).write(record.rid, record.key)
+            self.page_directory.get_base_tail(0).write(record.rid, record.key)
             for index in range(1, self.num_columns):
-                self.page_directory.get_tail(index).write(record.rid, record.columns[index-1])
+                self.page_directory.get_base_tail(index).write(record.rid, record.columns[index-1])
+            #version
+            self.page_directory.meta(record.rid)["max_rid"] = record.rid+1
         except MemoryError:
-            self.page_directory.expand()
-            self.page_directory.get_tail(0).write(record.rid, record.key)
+            #version_tail???
+            self.page_directory.meta(record.rid-1)["max_rid"] -= 1
+            self.page_directory.expand(record.rid)
+            self.page_directory.get_base_tail(0).write(record.rid, record.key)
             for index in range(1, self.num_columns):
-                self.page_directory.get_tail(index).write(record.rid, record.columns[index-1])
-
+                self.page_directory.get_base_tail(index).write(record.rid, record.columns[index-1])
         self.rid += 1
         pass
 
-    #TODO: If there are more than 5 pages, implement way to get proper page
-    # Problem would occur in get_tail(i), what if we want a previous page not the latest page?
+    """
+    * Function Name: read_record
+    * Purpose: Read record within page_directory
+    * Parameter: rid -> int
+    * Return: list of data, false if not found
+    """
+
     def read_record(self, rid):
         data = []
-        for i in range(self.num_columns):
-            data.append(self.page_directory.get_tail(i).read(rid))
-        return data
+        try:
+            for i in range(0, self.num_columns):
+                data.append(self.page_directory.get_base(rid, i).read(rid))
+            return data
+        except:
+            return False
+    
+    """
+    * Function Name: update_record
+    * Purpose: Update Record within page_directory
+    * Parameter: rid -> int, data -> List of desired data to insert
+    * Return: none, false if not found
+    """
+    def update_record(self, rid, data: list):
+        try:
+            # Update tail_v
+            self.page_directory.tail(rid).write(rid, data)    
+        #except MemoryError:
+        # implement error checking, if MemoryError -> Start Merge
+        #    print()
+        except:
+            return False
+
+    """
+    * Function Name: get_tail
+    * Purpose: Debugging function for tail pages
+    * Parameter: rid -> int
+    * Return: Tail page and the desired value in tail
+    """
+
+    # Debugger function for tail pages
+    def get_tail(self, rid):
+        tail = self.page_directory.tail(rid)
+        print(tail)
+        print(tail.read(rid))
+        pass
 
 
-#   FOR BASE PAGES
-#   5 columns
-#   i + (length_of_column - 1) * 5
-
-# --Indirection -----------------
-#   meta1       | Page 0  | Page 1 | and so on
-#   meta2       | Page 5  | Page 6
-#   meta3       | Page 10 | Page 11
-#   meta4       | Page 15 | Page 16
-#   meta5       | Page 20 | Page 21
+    """
+    * Function Name: get_meta
+    * Purpose: Debugging function for meta pages
+    * Parameter: rid -> int
+    * Return: Meta page of rid
+    """
+    # Debugger function for metapages
+    def get_meta(self, rid):
+        print(self.page_directory.meta(rid))
+        pass
