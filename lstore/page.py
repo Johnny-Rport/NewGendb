c91e5fb8f084fdd03ebf9e5b0d94df8a949c6970
Johnny <jar@overlord.lan>
2025-02-21 15:13:16 -0800

    Replaced Pickle

--- lstore/page.py
+++ lstore/page.py
@@ -1,4 +1,4 @@
-import pickle
+import msgpack
 
 class Page:
 
@@ -8,7 +8,7 @@ class Page:
 
     def has_capacity(self):
         # Returns size of dictionary as bytes
-        return len(pickle.dumps(self.data))
+        return len(msgpack.dumps(self.data))
 
     def write(self, rid, value):
         self.num_records += 1
@@ -17,7 +17,7 @@ class Page:
         size_data = self.has_capacity()
         
         # Size of value being inserted
-        size_value = len(pickle.dumps(value))
+        size_value = len(msgpack.dumps(value))
 
         if(size_data + size_value) >= 4096:
             raise MemoryError(f"Insufficient space in page for RID: {rid}")
