fc69ab8214f7f4cfb1a6ff0bd0b11972d76eb1fb
Terry <62485713+ctrterry@users.noreply.github.com>
2025-03-05 15:20:45 -0800

    Update Milestone3

--- lstore/transaction_worker.py
+++ lstore/transaction_worker.py
@@ -1,44 +1,50 @@
+
+#  * Program Name: Database System -> Milestone3 Update
+#  * Author: Next Genrartion
+#  * Date: Mar 05/2025 (Final_Version_v04)
+#  * Description:
 from lstore.table import Table, Record
 from lstore.index import Index
+import threading
 
 class TransactionWorker:
-
-    """
-    # Creates a transaction worker object.
-    """
-    def __init__(self, transactions = []):
-        self.stats = []
+    def __init__(self, transactions=None):
+        if transactions is None:
+            transactions = []
         self.transactions = transactions
+        self.stats = []
         self.result = 0
-        pass
+        self.worker_thread = None
 
-    
-    """
-    Appends t to transactions
-    """
     def add_transaction(self, t):
         self.transactions.append(t)
 
-        
-    """
-    Runs all transaction as a thread
-    """
     def run(self):
-        pass
-        # here you need to create a thread and call __run
-    
+        # Since, need to create a dedicated worker thread that runs all assigned transactions concurrently.
+        self.worker_thread = threading.Thread(target=self.__run)
+        self.worker_thread.start()
 
-    """
-    Waits for the worker to finish
-    """
     def join(self):
-        pass
-
+        if self.worker_thread:
+            self.worker_thread.join()
 
     def __run(self):
-        for transaction in self.transactions:
-            # each transaction returns True if committed or False if aborted
-            self.stats.append(transaction.run())
-        # stores the number of transactions that committed
-        self.result = len(list(filter(lambda x: x, self.stats)))
+        threads = []
+        results = [None] * len(self.transactions)
 
+        def run_transaction(i, transaction):
+            results[i] = transaction.run()
+
+        # spawn a thread for each transaction.
+        for i, transaction in enumerate(self.transactions):
+            t = threading.Thread(target=run_transaction, args=(i, transaction))
+            threads.append(t)
+            t.start()
+
+        # wait for all transaction threads and to finish.
+        for t in threads:
+            t.join()
+
+        self.stats = results
+        # Trying to count the number of transactions that successfully committed.
+        self.result = sum(1 for r in results if r)
